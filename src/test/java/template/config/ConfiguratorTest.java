59858bdae94e4e2ded0b277f1916e4420e582549
Mislav Jaksic <jaksicmislav@gmail.com>
2019-11-16 08:50:24 +0100

    Tested properties file paths

--- src/test/java/template/config/ConfiguratorTest.java
+++ src/test/java/template/config/ConfiguratorTest.java
@@ -1,20 +1,49 @@
 package template.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
 
 class ConfiguratorTest {
+	Configurator configurator = new Configurator();
+	
 	@Test
 	void getCliFilePropertiesTest() {
 		String file_path = "/app.properties";
-		Configurator configurator = new Configurator();
+		
 		Properties properties = configurator.getCliFileProperties(file_path);
 
 		assertEquals(properties.getProperty("app.name", "default not used"), "Properties Sample Code");
 		assertEquals(properties.getProperty("app.version"), "1.09");
 	}
+	
+	@Test
+	void streamPropertiesPathTest() {
+		Properties not_expected = new Properties();
+		
+		Properties actual = configurator.getCliFileProperties("/app.properties");
+		
+		assertNotEquals(not_expected, actual);
+	}
 
+	@Test
+	void log4jPropertiesPathTest() {
+		Properties not_expected = new Properties();
+		
+		Properties actual = configurator.getCliFileProperties("/log4j.properties");
+		
+		assertNotEquals(not_expected, actual);
+	}
+	
+	@Test
+	void testPropertiesPathTest() {
+		Properties not_expected = new Properties();
+		
+		Properties actual = configurator.getCliFileProperties("/test.properties");
+		
+		assertNotEquals(not_expected, actual);
+	}
 }
